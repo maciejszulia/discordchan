f01ba612ba49e62b5b950f812c93e9052387dede
maciejszulia <maciej.szulia@gmail.com>
2023-10-16 02:14:47 +0200

    0.1.0

--- discordchan.py
+++ discordchan.py
@@ -20,33 +20,27 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
 
 # Funkcja uruchamiana przy starcie bota
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} online at {datetime.datetime.now()}')
+    print(f'{bot.user.name} online at {datetime.datetime.now()}')   
     
     # Znajdź kanał o nazwie "#test-1" na serwerze
     channel = discord.utils.get(bot.get_all_channels(), name='test-1')
 
     if channel is not None:
         print('found channel')
-        # await channel.send(
+        # bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
         #     'online at ' + str(current_time) + '\n' +
         #     'current ver: ' + version + '\n' +
         #     github_link
         # )
     else:
         print('channel not found')
+        
 
-@bot.command()
-async def ping(ctx):
-    # Odpowiedz "pong" w kanale "#test-1"
-    channel = discord.utils.get(ctx.guild.channels, name='test-1')
-
-    if channel is not None:
-        await channel.send('pong')
 # Uruchom bota
 bot.run(TOKEN)
 
