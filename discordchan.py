305238b43a62650c4146b2817378d76126c0e498
maciejszulia <maciej.szulia@gmail.com>
2023-10-16 02:36:52 +0200

    0.1.1

--- discordchan.py
+++ discordchan.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import time
 import datetime
 import tokenhelper
 import versionhelper
@@ -10,7 +9,7 @@ print(tokenhelper.get_token())
 
 version = versionhelper.get_version()
 print(version)
-github_link = str("https://github.com/maciejszulia/discordchan")
+github_link = str("github.com/maciejszulia/discordchan")
 
 # current_time = datetime.datetime.now()
 print("Aktualny czas:", datetime.datetime.now())
@@ -27,16 +26,15 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=inten
 async def on_ready():
     print(f'{bot.user.name} online at {datetime.datetime.now()}')   
     
-    # Znajdź kanał o nazwie "#test-1" na serwerze
-    channel = discord.utils.get(bot.get_all_channels(), name='test-1')
+    # Znajdź kanał o nazwie "#test" na serwerze
+    channel = discord.utils.get(bot.get_all_channels(), name='test')
 
     if channel is not None:
-        print('found channel')
-        # bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
-        #     'online at ' + str(current_time) + '\n' +
-        #     'current ver: ' + version + '\n' +
-        #     github_link
-        # )
+        await channel.send(
+            'online at ' + str(datetime.datetime.now()) + '\n' +
+            'current ver: ' + version + '\n' +
+            github_link
+        )
     else:
         print('channel not found')
         
