8279372c3c484b9176bfd9525327dd62da06074b
maciejszulia <maciej.szulia@gmail.com>
2023-11-29 12:02:06 +0100

    version_updater.py done

---
+++ todo.md
@@ -0,0 +1,11 @@
+# todo
+
+- ksalol-chan.py
+  - ogarnac co jest nie tak z tym tokenem
+- version_updater.py
+  - ma sie autoupdejtowac jak wepchne do gita
+  - changelog?
+- testing env
+  - rename
+- version_updater.py
+  - ma dodawac date a potem to wjebac do changeloga
--- version_updater.py
+++ version_updater.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+
 def update_version():
     try:
         with open('.git/COMMIT_EDITMSG', 'r') as commit_file:
             commit_lines = commit_file.readlines()
 
-        stripped_lines = [line.lstrip('# ').rstrip() + '\n' if line.startswith('#') else line for line in commit_lines]
+        stripped_lines = [
+            f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} {line.lstrip("# ").rstrip()}\n' 
+            if line.startswith('#') 
+            else f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n{line}'
+            for line in commit_lines
+        ]
 
         with open('version', 'a') as version_file:
             version_file.write(''.join(stripped_lines))
